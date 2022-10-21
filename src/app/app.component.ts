3be909705303e798813583a2101a78ed23699419
Julia Ju <Shinyjlf@gmail.com>
2022-10-21 16:33:24 +0300

    broken validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { defineLocale } from 'ngx-bootstrap/chronos';
@@ -8,6 +8,7 @@ import { ruLocale } from 'ngx-bootstrap/chronos';
 
 import { ListItem } from "./listItem";
 import { DataService } from "./data.service";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +19,11 @@ export class AppComponent implements OnInit {
 
   minDate: Date;
   maxDate: Date;
-  //mytime: Date;
-  fmGroup: any;
+  //bookedDateTime: Date | undefined = new Date();
+  fmGroup: FormGroup; avtoNumber_class: boolean = false; IsInvalidClass: boolean[];
   ServiceStationItems: Observable<ListItem[]>;
   AvtoMakeItems: Observable<ListItem[]>;
   AvtoModelItems: Observable<ListItem[]>;
-
 
 
   constructor(private formbuilder: FormBuilder, private DataService: DataService, private localeService: BsLocaleService) {
@@ -33,31 +33,125 @@ export class AppComponent implements OnInit {
     this.maxDate = new Date();
     this.minDate.setDate(this.minDate.getDate());
     this.maxDate.setDate(this.minDate.getDate() + 31);
-
     //this.mytime = new Date();
-    //this.mytime.setHours(0, 0);
-    
+    //this.mytime.setHours(0, 0);    
   }
 
-  AvtoModelItems_Fill() {
-    this.AvtoModelItems = this.DataService.sendRequest_GetModels(this.fmGroup.get('avtoMake').value);
+  AvtoModelItems_Fill(avtoMakeID: string) {
+    this.AvtoModelItems = this.DataService.sendRequest_GetModels(avtoMakeID);
   }
+
+  CarData_Fill() {
+
+    if (this.fmGroup.get('avtoNumber').valid) {
+      this.avtoNumber_class = false;
+      //this.DataService.sendRequest_Login().subscribe(res => {
+      this.DataService.sendRequest_GetCarData(this.fmGroup.get('avtoNumber').value).subscribe(res => {
+        console.log(res["result"]["Response"]);
+        let i = 0; let _customerID = ""; let _avtoModelID = ""; let _avtoVIN = ""; let _customerPhone = ""; let _customerName = ""; let _avtoMakeID = "";
+        console.log("received avto data = " + res["result"]["Response"]["CarsByGosreg"]["data"]);
+
+        res["result"]["Response"]["CarsByGosreg"]["data"].map(item => {
+          _customerID = item.PROPERTY_CUSTOMER_ID;
+          _avtoModelID = item.MODEL_ID;
+          _avtoVIN = item.VIN;
+          this.fmGroup.patchValue({ avtoVIN: _avtoVIN });
+          i++;
+          if (_customerID != "") {
+            this.DataService.sendRequest_GetCustomerData(_customerID).subscribe(res1 => {
+              console.log("customer by id");
+              console.log(res1["result"]["Response"]);
+
+              let data = res1["result"]["Response"]["Customer"]["data"];
+              _customerPhone = data["PHONE"] ?? '';
+              _customerName = (data["FIRST_NAME"] ?? '') + ' ' + (data["SECOND_NAME"] ?? '') + ' ' + (data["LAST_NAME"] ?? '');
+              this.fmGroup.patchValue({ clientName: _customerName, clientPhone: _customerPhone });
+
+            });
+          }
+
+          if (_avtoModelID != "") {
+            this.DataService.sendRequest_GetCarModelData(_avtoModelID).subscribe(res1 => {
+              console.log("car model by id");
+              console.log(res1["result"]["Response"]);
+              let data = res1["result"]["Response"]["Model"]["data"];
+              let _avtoMakeID = data["MARK_ID"] ?? '';
+              if (_avtoMakeID != "") { //can't be empty
+                this.AvtoModelItems = this.DataService.sendRequest_GetModels(_avtoMakeID).pipe(map(res2 => {
+                  console.log('model = ' + _avtoModelID + ', makeId= ' + _avtoMakeID);
+                  this.fmGroup.patchValue({ avtoMake: _avtoMakeID, avtoModel: _avtoModelID });
+                  return res2;
+                }));
+              }
+              
+            });
+
+          }
+
+        });
+
+        if (i == 0) {
+          this.DataService.sendRequest_GetCarDataExtended(this.fmGroup.get('avtoNumber').value).subscribe(res1 => {
+            console.log("car by avto numberb (gibdd)");
+            console.log(res1["result"]["Response"]);
+            let data = res1["result"]["Response"]["rsaExtended"];
+            _customerName = data["ownerFio"] ?? '';
+            let _avtoMake :string = data["carMark"] ?? '';
+            let _avtoModel :string = data["carModel"] ?? '';
+            _avtoVIN = data["vin"] ?? '';
+            this.fmGroup.patchValue({ clientName: _customerName, avtoVIN: _avtoVIN });
+
+            this.AvtoMakeItems.forEach(itm => {                          
+              let ls_item = itm.find(a => a.name.toLowerCase().trim() == _avtoMake.toLowerCase().trim());
+              console.log(ls_item.id);
+              _avtoMakeID = ls_item.id;
+              this.fmGroup.patchValue({ avtoMake: _avtoMakeID });
+
+              if (_avtoMakeID != "") {
+                this.AvtoModelItems = this.DataService.sendRequest_GetModels(_avtoMakeID).pipe(map(res2 => {
+                  _avtoModelID = res2.find(itm => (itm.name.toLowerCase().trim() == _avtoModel.toLowerCase().trim()))?.id;
+                  console.log('model = ' + _avtoModelID);
+                  this.fmGroup.patchValue({ avtoModel: _avtoModelID });
+                  return res2;
+                }));
+              }
+
+            });
+            
+            
+
+            
+            
+
+          });
+        }
+
+      });
+      //});
+    }
+    else {
+      this.avtoNumber_class = true;
+    }
+  }
+
+ 
 
   ngOnInit() {
 
     this.fmGroup = this.formbuilder.group({
-      avtoNumber: ['', Validators.required],
-      clientPhone: ['', Validators.required],
+      avtoNumber: ['', [Validators.required, Validators.pattern(/^\D\d{3}\D{2}\d{2,3}$/)]], //В455КВ197
+      clientPhone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
       clientName: ['', Validators.required],
       serviceStation: ['', Validators.required],
       avtoMake: ['', Validators.required],
       avtoModel: ['', Validators.required],
+      avtoVIN: [''],
       avtoWorks: ['', Validators.required],
       agreeOnTheCost: [true, Validators.required],
       bookedDate: ['', Validators.required],
       bookedTime: ['', ]
-    }); //this.fmGroup.setValue({avtoNumber: 'Carson', clientPhone: 'Drew'});
-
+    });
+    
     this.DataService.sendRequest_Login().subscribe(res => {
       console.log("login");
       this.ServiceStationItems = this.DataService.sendRequest_GetServiceStations();
@@ -67,13 +161,26 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit(): void {
-    this.DataService.sendRequest_SaveBooking(this.fmGroup).subscribe(res => {
-      console.log("saved:");
-      console.log(res);
+   // if (this.fmGroup.valid) {
+      this.DataService.sendRequest_SaveBooking(this.fmGroup).subscribe(res => {
+        console.log("saved:");
+        console.log(res);
+      });
 
-    });
-      
+   // }
   }
+
+  setValidity(name: string) {
+    this.IsInvalidClass[name] = this.fmGroup.get(name).valid;    
+  }
+
+  get avtoNumber() {
+    return this.fmGroup.get('avtoNumber');
+  }
+  get avtoMake() {
+    return this.fmGroup.get('avtoMake');
+  }
+
 
   
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -15,7 +15,7 @@ export class DataService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public sendRequest_Login() { //https://TEH_TEST:a1b2c3D$@zenon.basgroup.ru:55724/api-v2/auth/login?API_KEY=92ae9e4c96394cf2aa2f462a5fde3b19     //TEH_TEST:a1b2c3D$@
+  public sendRequest_Login(): Observable<any> { //https://TEH_TEST:a1b2c3D$@zenon.basgroup.ru:55724/api-v2/auth/login?API_KEY=92ae9e4c96394cf2aa2f462a5fde3b19     //TEH_TEST:a1b2c3D$@
     console.log(" login function"); 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -33,9 +33,41 @@ export class DataService {
     return results;
   }
 
+  public sendRequest_GetCarData(carNumber: string): Observable<any> {
+    let results = this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/CarsByGosreg/" + carNumber + "?SESSIONID=" + this.SessionID).pipe(catchError(this.handleError), map(data => {
+      console.log("=getting car data 1=");
+      console.log(data);
+      return data;
+    }));
+    return results;
+  }
 
-  
+  public sendRequest_GetCarDataExtended(carNumber: string): Observable<any> {
+    let results = this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/ExtRequests/rsaExtended?GOSREG=" + carNumber + "&SESSIONID=" + this.SessionID).pipe(catchError(this.handleError), map(data => {
+      console.log("=getting  car data 2=");
+      console.log(data);
+      return data;
+    }));
+    return results;
+  }
 
+  public sendRequest_GetCarModelData(modelID: string): Observable<any> {
+    let results = this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Model/" + modelID + "?SESSIONID=" + this.SessionID).pipe(catchError(this.handleError), map(data => {
+      console.log("=getting model data 1=");
+      console.log(data);
+      return data;
+    }));
+    return results;
+  }
+  public sendRequest_GetCustomerData(customerID: string): Observable<any> {
+    let results = this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Customer/" + customerID + "?SESSIONID=" + this.SessionID).pipe(catchError(this.handleError), map(data => {
+      console.log("=getting customer data 1=");
+      console.log(data.toString());
+      console.log(data);
+      return data;
+    }));
+    return results;
+  }
 
   public sendRequest_GetServiceStations(): Observable<ListItem[]> {
     return this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/RW/Directories/LDepartmentsCombo?SESSIONID=" + this.SessionID)
@@ -60,8 +92,8 @@ export class DataService {
       }));
   }
 
-  public sendRequest_GetModels(modelId: string): Observable<ListItem[]> {
-    return this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Models?FilterString=MARK_ID=?&FilterParam[0]=" + modelId + "&Fields[_]=MODEL_ID,MODEL_NAME&SESSIONID=" + this.SessionID)
+  public sendRequest_GetModels(markId: string): Observable<ListItem[]> {
+    return this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Models?FilterString=MARK_ID=?&FilterParam[0]=" + markId + "&Fields[_]=MODEL_ID,MODEL_NAME&SESSIONID=" + this.SessionID)
       .pipe(catchError(this.handleError), map(res => {
       console.log(res["result"]["Response"]);
       let results = res["result"]["Response"]["Models"]["data"].map(item => {
@@ -73,38 +105,36 @@ export class DataService {
 
   public sendRequest_SaveBooking(form: any): Observable<any>{
     console.log(form.value);
+
     let date = new Date(form.get('bookedDate').value);
-    let tt = form.get('bookedDate').value;
-    let hh = form.get('bookedTime').value;
-
     let time = new Date(form.get('bookedTime').value);
-    let start_datetime = new Date(date.toDateString() + ' ' + date.toTimeString());
-    console.log('datetime= ' + start_datetime.toString());
-
+    let start_datetime = new Date(date.toDateString() + ' ' + time.toTimeString());
+   
     let body = {
       LDEPARTMENT_ID: form.get('serviceStation').value,
       CREATE_DATE: new Date(),
-      START: date,
+      START: start_datetime,
       CLIENT_NAME: 'Василий Иванович Чапаев', // form.get('clientName').value
       CLIENT_PHONE: '+70001111111', //form.get('clientPhone').value
       GOSREG: form.get('avtoNumber').value,
+      VIN: form.get('avtoVIN').value,
       MODEL_ID: form.get('avtoModel').value,
       COMMENT: form.get('avtoWorks').value
     };
     console.log(body);
-    return this.httpClient.post("https://" + this.REST_API_SERVER + "/api-v2/Core/RW/ScheduledCar?SESSIONID=" + this.SessionID, body).pipe(catchError(this.handleError));
+    return of([]); //this.httpClient.post("https://" + this.REST_API_SERVER + "/api-v2/Core/RW/ScheduledCar?SESSIONID=" + this.SessionID, body).pipe(catchError(this.handleError));
 
   }
-  public sendRequest_CheckSaving(serviceStationId: string, date: Date): Observable<ListItem[]> {
-    return this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Models?FilterString=MARK_ID=?&FilterParam[0]=" + modelId + "&Fields[_]=MODEL_ID,MODEL_NAME&SESSIONID=" + this.SessionID)
-      .pipe(catchError(this.handleError), map(res => {
-        console.log(res["result"]["Response"]);
-        let results = res["result"]["Response"]["Models"]["data"].map(item => {
-          return new ListItem(item.MODEL_ID, item.MODEL_NAME);
-        })
-        return results;
-      }));
-  }
+  //public sendRequest_CheckSaving(serviceStationId: string, date: Date): Observable<ListItem[]> {
+  //  return this.httpClient.get("https://" + this.REST_API_SERVER + "/api-v2/Core/Directories/Models?FilterString=MARK_ID=?&FilterParam[0]=" + modelId + "&Fields[_]=MODEL_ID,MODEL_NAME&SESSIONID=" + this.SessionID)
+  //    .pipe(catchError(this.handleError), map(res => {
+  //      console.log(res["result"]["Response"]);
+  //      let results = res["result"]["Response"]["Models"]["data"].map(item => {
+  //        return new ListItem(item.MODEL_ID, item.MODEL_NAME);
+  //      })
+  //      return results;
+  //    }));
+  //}
 
   handleError(error: HttpErrorResponse) {
     let errorMessage = 'Unknown error!';
